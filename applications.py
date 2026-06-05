bba1f157515266eeb810cba257f8b698bb8efd30
PRATIKMAHALLE <52369000+PRATIKMAHALLE@users.noreply.github.com>
2026-06-06 03:11:57 +0530

    Update applications.py

--- applications.py
+++ applications.py
@@ -18,7 +18,7 @@ def get_db_connection():
         mydb = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="Pratik28@", 
+            password="Pratik28@",
             database='querysystem',
             autocommit=True
         )
